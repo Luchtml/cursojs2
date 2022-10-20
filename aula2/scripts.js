705ed2ad02d879596b35b243673cf3d96e0eb6f3
Lucas Costa <luchtml5@gmail.com>
2022-10-20 09:04:11 -0300

    Update scripts.js

--- aula2/scripts.js
+++ aula2/scripts.js
@@ -18,3 +18,18 @@ function sair() {
     alert ('Até mais')
     area.innerHTML = 'Você saiu!'
 }
+
+function mediaAluno(nota1, nota2){
+    let media = (nota1 + nota2) / 2
+    if(media >= 7) {
+        console.log('Aluno Aprovado com a média:' + ' ' + media)
+    } else if(media < 7) {
+        console.log('Aluno reprovado com a média:' + ' ' + media)
+    }
+}
+
+function aluno(nome, curso) {
+    let msg = 'Seja Bem vindo(a) ' + nome + ' ao curso de ' + curso
+
+    console.log(msg)
+}
