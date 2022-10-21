1999c4851190049b02b61281de7b40638358fe57
Lucas Costa <luchtml5@gmail.com>
2022-10-21 09:57:57 -0300

    aula  e 5

--- aula4/scripts.js
+++ aula4/scripts.js
@@ -1,29 +1,16 @@
-let peso
-let altura
-let imc
-let resultado
-
-
-function calcular(event) {
-    event.preventDefault()
-    peso = document.querySelector('#peso').value
-    altura = document.querySelector('#altura').value
-    imc = peso / (altura*altura)
-    if(imc < 17) {
-        resultado = document.querySelector('#resultado')
-        resultado.innerHTML = '<br> Seu resultado foi: ' + imc + '<br> Cuidado, você está muito abaixo do peso'
-    } else if(imc > 17 && imc <= 18.49) {
-        resultado = document.querySelector('#resultado')
-        resultado.innerHTML = '<br> Seu resultado foi: ' + imc + '<br> Você está abaixo do peso!'
-    } else if(imc >= 18.5 && imc <= 24.99) {
-        resultado = document.querySelector('#resultado')
-        resultado.innerHTML = '<br> Seu resultado foi: ' + imc + '<br> Você está no peso ideal'
-    } else if(imc > 25 && imc <= 29.99) {
-        resultado = document.querySelector('#resultado')
-        resultado.innerHTML = '<br> Seu resultado foi: ' + imc + '<br> Cuidado, você está acima do peso'
-    } else if (imc >= 30) {
-        resultado = document.querySelector('#resultado')
-        resultado.innerHTML = '<br> Seu resultado foi: ' + imc + '<br> Cuidado, obesidade'
-    }
-
+// //setInterval
+function acao(){
+    document.write('Executando...<br>')
 }
+
+
+// let timer = setInterval(() => {
+//     document.write('Executando!!! <br>')
+// }, 1000)
+
+//setTimeout
+
+// setTimeout(acao, 3000)
+setTimeout(() => {
+    console.log('Executou')
+}, 3000)
