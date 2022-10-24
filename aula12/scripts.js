e00ac21642b39dfc31e37ade38a91f5bf48b7ca3
Lucas Costa <luchtml5@gmail.com>
2022-10-24 17:05:51 -0300

    Update scripts.js

---
+++ aula12/scripts.js
@@ -0,0 +1,15 @@
+// //includes, starsWith, endsWith
+
+// let nomes = ['Lucas', 'Matheus', 'Joao']
+
+// console.log(nomes.includes('maria'))
+
+// if (nomes.includes('Maria')) {
+//     console.log('Está na lista')
+// } else {
+//     console.log('Não está na lista')
+// }
+
+let nome = 'Matheus'
+
+console.log(nome.startsWith('Mat'))
