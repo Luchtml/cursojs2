9f9ee14204b6e3e48d1ee343c6c2bf50a15eba00
Lucas Costa <luchtml5@gmail.com>
2022-11-02 11:17:20 -0300

    prototype

---
+++ aula15/scripts.js
@@ -0,0 +1,21 @@
+function Pessoa(nome, idade){
+    this.nome = nome
+    this.idade = idade
+    this.abracar = function(){
+        return 'Abraçou'
+    }
+    this.andar = function(){
+        return 'Andou pelo objeto'
+    }
+}
+
+Pessoa.prototype.andar = function(){
+    return this.nome + ' andou'
+}
+Pessoa.prototype.nadar = function(){
+    return this.nome + ' nadou'
+}
+
+const lucas = new Pessoa('Lucas', 26)
+console.log(Pessoa.prototype)
+console.log(lucas.prototype)
