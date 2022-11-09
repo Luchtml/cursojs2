c152e3fc57347a067294952e6804768614322b78
Lucas Costa <luchtml5@gmail.com>
2022-11-09 11:02:38 -0300

    number e math

--- desafios/desafio_numeros/scripts.js
+++ desafios/desafio_numeros/scripts.js
@@ -1,6 +1,5 @@
 //Retorne um número aleatório entre 1050 e 2000
-
-const aleatorio = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050);
+const aleatorio = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050)
 
 console.log(aleatorio);
 
@@ -8,11 +7,9 @@ console.log(aleatorio);
 
 //Retorne o maior número da lista abaixo
 const numeros = "4, 5, 20, 40, 9";
-
-const arrayNumeros = numeros.split(", ");
-const numeroMaximo = Math.max(...arrayNumeros)
-console.log(numeroMaximo);
-
+const separaNumero = numeros.split(', ')
+const numeroMaximo = Math.max(...separaNumero)
+console.log(numeroMaximo)
 
 
 
@@ -23,3 +20,18 @@ console.log(numeroMaximo);
 
 const listaPrecos = ['R$ 59,99', ' R$ 100,222',
                      'R$ 230', 'r$  200']
+
+
+function limparPreco (preco) {
+    preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.')
+    preco.toFixed(2)
+    return preco
+}
+let soma = 0
+listaPrecos.forEach((preco) => {
+    soma += limparPreco(preco)
+})
+
+console.log(soma.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))
+
+limparPreco(listaPrecos[1])
