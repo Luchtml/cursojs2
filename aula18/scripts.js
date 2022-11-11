919d4766fe54c81c66aab8fe5133f5a61a663514
Lucas Costa <luchtml5@gmail.com>
2022-11-11 10:12:44 -0300

    Update scripts.js

---
+++ aula18/scripts.js
@@ -0,0 +1,49 @@
+// const carros = ['Ford', 'Fiat', 'Honda']
+
+// carros.forEach((item, index, array) => {
+//     array[index] = 'teste'
+//     console.log(item, index, array)
+// })
+
+// const li = document.querySelectorAll('li')
+
+// li.forEach((i, index) => i.classList.add('ativa' + index))
+
+
+
+// const carros = ['Ford', 'Fiat', 'Honda']
+
+// const newCarros = carros.map((item, index, array) => {
+//     return item.toUpperCase()
+    
+// })
+
+// const numeros = [2, 4, 5, 6, 78]
+
+// const numerosX2 = numeros.map((n) => n * 2)
+
+
+// console.log(numerosX2)
+// console.log(newCarros)
+
+
+
+
+// const aulas = [10, 25, 30]
+
+// const reduceAulas = aulas.reduce((acumulador, item) => { 
+//     console.log(acumulador, item)
+//     return acumulador + item
+// })
+
+
+// console.log(reduceAulas)
+
+// const numeros = [10, 25, 30, 3, 54, 33, 22]
+
+// const maiorNumero = numeros.reduce((anterior, atual) => 
+//  anterior > atual ? anterior : atual
+// , 0)
+
+// console.log(maiorNumero)
+
