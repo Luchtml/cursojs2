4c1b1acfde3a77b0b0a9b75437798b7716d34ba7
Lucas Costa <luchtml5@gmail.com>
2022-11-14 15:24:53 -0300

    estudando tooltip

--- efeitos-no-dom/modal/js/script.js
+++ efeitos-no-dom/modal/js/script.js
@@ -3,9 +3,12 @@ import initAnimacaoScroll from './modules/animacao-scroll.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tabnav.js'
 import initModal from './modules/modal.js'
+import toolTip from './modules/tooltip.js'
+import initToolTip from './modules/tooltip.js'
 
 initModal()
 initScrollSuave()
 initAnimacaoScroll()
 initAccordion()
 initTabNav()
+initToolTip()
