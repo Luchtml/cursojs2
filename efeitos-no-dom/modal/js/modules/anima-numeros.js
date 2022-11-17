193fe4991081961b5e5b0f291c70a8a281a2a913
Lucas Costa <luchtml5@gmail.com>
2022-11-17 20:26:21 -0300

    animação de numeros

--- efeitos-no-dom/modal/js/modules/anima-numeros.js
+++ efeitos-no-dom/modal/js/modules/anima-numeros.js
@@ -1,18 +1,30 @@
 export default function initAnimaNumeros() {
+  function animaNumeros() {
+    const numeros = document.querySelectorAll("[data-numero]");
 
-}
-
-const numeros = document.querySelectorAll('[data-numero]')
-
-numeros.forEach((numero => {
-    const total = +numero.innerText
-    
-    let start = 0
-    const timer = setInterval(() => {
-        start++
-        numero.innerText = start
+    numeros.forEach((numero) => {
+      const total = +numero.innerText;
+      const incremento = Math.floor(total / 100);
+      let start = 0;
+      const timer = setInterval(() => {
+        start += incremento;
+        numero.innerText = start;
         if (start > total) {
-            clearInterval(timer)
+          numero.innerText = total;
+          clearInterval(timer);
         }
-    }, 0)
-}))
+      }, 50 * Math.random());
+    });
+  }
+
+  function handleMutation(mutation) {
+    if (mutation[0].target.classList.contains("ativo")) {
+      observer.disconnect();
+      animaNumeros();
+    }
+  }
+  const observerTarget = document.querySelector(".numeros");
+  const observer = new MutationObserver(handleMutation);
+
+  observer.observe(observerTarget, { attributes: true });
+}
