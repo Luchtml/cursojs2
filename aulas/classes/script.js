fdecb01e12f5ec4195d670b8b822485f618425bb
Lucas Costa <luchtml5@gmail.com>
2022-11-22 18:25:02 -0300

    classes

---
+++ aulas/classes/script.js
@@ -0,0 +1,15 @@
+function Button(text, background) {
+    this.text = text
+    this.background = background
+}
+
+Button.prototype.element = function () {
+    const buttonElement = document.createElement('button')
+    buttonElement.innerText = this.text
+    buttonElement.background = this.background
+    return buttonElement
+}
+
+const blueButton = new Button('Comprar', 'blue')
+
+console.log(blueButton.element())
