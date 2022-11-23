efc6d400d3f6fb7dd01e4d289483431cae9cd6d6
Lucas Costa <luchtml5@gmail.com>
2022-11-23 18:34:03 -0300

    Update script.js

--- aulas/classes-3/script.js
+++ aulas/classes-3/script.js
@@ -8,9 +8,18 @@ class Veiculo {
 }
 
 class Moto extends Veiculo {
+    constructor (rodas, capacete) {
+        super(rodas)
+        this.capacete = capacete
+    }
+    acelerar() {
+        super.acelerar()
+        console.log('acelerou rapido')
+    }
     empinar() {
-        console.log('Moto empinou com ' + this.rodas + 'rodas')
+        console.log('Moto empinou com ' + this.rodas + ' rodas')
     }
 }
 
-const honda = new Moto(2)
+const honda = new Moto(2, true)
+const civic = new Veiculo(4)
