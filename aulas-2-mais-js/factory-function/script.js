0939e48836a35a36bfa965d7056cfbbfc8ff3407
Lucas Costa <luchtml5@gmail.com>
2022-11-24 11:08:32 -0300

    Update script.js

--- aulas-2-mais-js/factory-function/script.js
+++ aulas-2-mais-js/factory-function/script.js
@@ -1,7 +1,65 @@
-function createButton(text){
-    return{
-        text: text,
-    }
+// function createButton(text){
+
+//     const numeroSecreto = 'j2342mn4324çasd'
+
+//     function element() {
+//         const btnElement = document.createElement('button')
+//         btnElement.innerText = text
+//         return btnElement
+//     }
+
+//     return{
+//         text,
+//         element,
+//         numeroSecreto,
+//     }
+// }
+
+// const btnComprar = createButton('Comprar')
+// const btnVender = createButton('Vender')
+
+// console.log(btnComprar, btnVender)
+
+
+
+///////////////////////////////////////////////////////////
+
+// function createButton(text){
+
+
+//     function element() {
+//         const btnElement = document.createElement('button')
+//         btnElement.innerText = text
+//         return btnElement
+//     }
+
+//     return Object.freeze({
+//         text,
+//         element,
+//     })
+// }
+
+// const btnComprar = createButton('Comprar')
+// const btnVender = createButton('Vender')
+
+// btnComprar.text = 'Novo Texto'
+// btnComprar.element = 'Novo Texto'
+
+// console.log(btnComprar, btnVender)
+
+function Pessoa(nome) {
+    // if(!(this instanceof Pessoa))
+    //forma nova do ES6
+    if(!new.target)
+        return new Pessoa(nome)
+    
+   this.nome = nome
 }
 
-const btnBlue = createButton('Comprar')
+Pessoa.prototype.andar = () => {
+  return `${this.nome} andou`
+}
+
+const designer = Pessoa('Lucas')
+
+console.log(designer)
