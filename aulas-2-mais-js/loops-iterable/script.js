536852179ea90b385509ce040d6b1865cc7bfd9c
Lucas Costa <luchtml5@gmail.com>
2022-11-30 14:17:07 -0300

    Update script.js

--- aulas-2-mais-js/loops-iterable/script.js
+++ aulas-2-mais-js/loops-iterable/script.js
@@ -1,5 +1,37 @@
 const frutas = ['Bana', 'Morango', 'Uva']
 const frase = 'Isso é JavaScript'
 
-fetch('https://pokeapi.co/api/v2/pokemon/')
-.then(({headers}) => console.log(headers))
+for(const fruta of frutas) {
+    console.log(fruta)
+}
+// for(const char of frase) {
+//     console.log(char)
+// }
+
+const buttons = document.querySelectorAll('button')
+
+for(const btn of buttons) {
+    btn.style.color = 'blue'
+}
+
+console.log(...buttons)
+
+
+// fetch('https://pokeapi.co/api/v2/pokemon/')
+// .then(({headers}) => console.log(headers))
+
+const carro = {
+    marca: 'Honda',
+    ano: 2018,
+}
+
+// for(const chave of carro) {
+//     console.log(chave)
+// }
+                            // [ERRO]
+
+
+
+for(const propriedade in carro) {
+    console.log(propriedade, carro[propriedade])
+}
