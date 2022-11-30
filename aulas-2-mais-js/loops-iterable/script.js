a992236248cecc254de279ec0d921324f7f5709a
Lucas Costa <luchtml5@gmail.com>
2022-11-30 15:39:27 -0300

    exercicio

--- aulas-2-mais-js/loops-iterable/script.js
+++ aulas-2-mais-js/loops-iterable/script.js
@@ -1,7 +1,10 @@
 // crie 4 li's na página utilizando for ... of adicione uma classe a cada li
 
+const itens = document.querySelectorAll('li')
 
-
+for(const item of itens) {
+    item.classList.add('ativo')
+}
 
 
 
@@ -15,3 +18,6 @@
 
 //utilize o for... in para lista todos as propriedades e valores do objeto window
 
+for(const propriedade in window) {
+    console.log(propriedade + ': ' + window[propriedade])
+}
