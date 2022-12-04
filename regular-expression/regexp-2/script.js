52219dc03d697715bc1bd05d1b523c545393f578
Lucas Costa <luchtml5@gmail.com>
2022-12-04 09:47:45 -0300

    Update script.js

---
+++ regular-expression/regexp-2/script.js
@@ -0,0 +1,10 @@
+const cpfs = document.querySelectorAll('.cpf li')
+
+function elementsInnerText(elements) {
+    const arrayElements = Array.from(elements)
+    return arrayElements.map(element => {
+        return element.innerText 
+    })
+}
+
+console.log(elementsInnerText(cpfs))
