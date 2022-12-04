4b335dd542037f00e4ccfdc41479d543448da59a
Lucas Costa <luchtml5@gmail.com>
2022-12-04 10:06:43 -0300

    Update script.js

--- regular-expression/regexp-2/script.js
+++ regular-expression/regexp-2/script.js
@@ -1,10 +1,51 @@
-const cpfs = document.querySelectorAll('.cpf li')
+// const cpfs = document.querySelectorAll('.cpf li')
 
-function elementsInnerText(elements) {
-    const arrayElements = Array.from(elements)
-    return arrayElements.map(element => {
+
+// function elementsInnerText([...elements]) {
+//     return elements.map(element => {
+//         return element.innerText 
+//     })
+// }
+
+// console.log(elementsInnerText(cpfs))
+
+
+
+// funcão para pegar toda uma nodelist e converter em array de texto
+
+
+
+const cpfsList = document.querySelectorAll('.cpf li')
+
+
+const elementsInnerText = ([...elements]) => {
+    return elements.map(element => {
         return element.innerText 
     })
 }
 
-console.log(elementsInnerText(cpfs))
+const limparCpf = (cpf) => {
+    return cpf.replace(/\D/g, '')
+}
+
+const construirCpf = (cpf) => {
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4')
+}
+
+const formatarCpfs = (cpfs) => {
+    return cpfs.map(limparCpf).map(construirCpf)
+}
+
+const substituirCpfs = (cpfsElements) => {
+    const cpfs = elementsInnerText(cpfsElements)
+    const cpfsFormatados = formatarCpfs(cpfs)
+
+    cpfsElements.forEach((element, index) => {
+        element.innerText = cpfsFormatados[index]
+    })
+}
+
+
+
+substituirCpfs(cpfsList)
+
