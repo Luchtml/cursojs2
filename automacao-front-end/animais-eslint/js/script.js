c2f29106cf1784cfdd79b1e32d1deb3e767684b2
Lucas Costa <luchtml5@gmail.com>
2022-12-07 10:14:45 -0300

    est

---
+++ automacao-front-end/animais-eslint/js/script.js
@@ -0,0 +1,23 @@
+import initScrollSuave from './modules/scroll-suave.js'
+import initAnimacaoScroll from './modules/animacao-scroll.js'
+import initAccordion from './modules/accordion.js'
+import initTabNav from './modules/tabnav.js'
+import initModal from './modules/modal.js'
+import initDropdownMenu from './modules/dropdown-menu.js'
+import initToolTip from './modules/tooltip.js'
+import initMenuMobile from './modules/menu-mobile.js'
+import initFuncionamento from './modules/funcionamento.js'
+import initFetchAnimais from './modules/fetchanimais.js'
+import initFetchBitcoin from './modules/fech-bitcoin.js'
+
+initModal()
+initScrollSuave()
+initAnimacaoScroll()
+initAccordion()
+initTabNav()
+initToolTip()
+initDropdownMenu()
+initMenuMobile()
+initFuncionamento()
+initFetchAnimais()
+initFetchBitcoin()
